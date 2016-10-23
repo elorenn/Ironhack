53aa3bd9d57e87f14f960ea4abb5269b495dc8e6
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-10-23 01:32:03 -0400

    My board knows 1)ther is no chess piece, 2) not changing positions, 3) trying to move off board, BUT need to fix glitch in accessing individual chess piece's can_move? method.

--- Week_1/Day_5/chess_validator/lib/board.rb
+++ Week_1/Day_5/chess_validator/lib/board.rb
@@ -5,6 +5,8 @@ require_relative("bishop.rb")
 require_relative("queen.rb")
 require_relative("pawn.rb")
 
+require_relative("chesspiece.rb") 
+
 class Board 
 
 	def initialize
@@ -12,21 +14,44 @@ class Board
 	end
 
 	def add_chess_piece(chess_piece)
-		@chess << (chess_piece) 
+		@chess[chess_piece.starting_x][chess_piece.starting_y] = chess_piece
 	end 
 
 	def board_can_move?(starting_x, starting_y, ending_x, ending_y)
+		
+# check if there's a piece:
+		if @chess[starting_x][starting_y] == nil 
 
-		# 1) check if there's a piece
-		# 2) If the piece is there, call that piece's .can_move? method
-		# 3) If the piece can move, check to see if the move stays within the board's bundaries.
-		# 4) Check if the piece is actually trying to move (i.e. the destination is not the same as the origin)
+			puts "There is no chess piece at #{starting_x}, #{starting_y}."
 
-		# optional bonus 5) make sure there is no piece already in the destination
+		elsif ending_x == starting_x && ending_y == starting_y
+			
+			puts "The piece is not changing positions. Please enter a new destination."
 
-	end
+		elsif ending_x >= 9 || ending_y >= 9
+				
+			puts "You're trying to move the piece off the board. Please enter a new destination."	
 
+		elsif (ending_x < 9 || ending_y < 9) && (@chess[starting_x][starting_y].can_move?(ending_x, ending_y)) 
+					
+			puts "The piece can move from #{starting_x}, #{starting_y} to #{ending_x}, #{ending_y}."
 
-# optional bonus: move the piece
+		elsif   
 
+			puts "The piece cannot move there." 
+
+		end		
+	end		
 end
+
+# check to see if the move stays within the board's bundaries: 
+			
+# check if the piece is actually trying to move: 
+				
+# call that piece's .can_move? method:
+				
+
+
+# bonus: move the piece
+
+
