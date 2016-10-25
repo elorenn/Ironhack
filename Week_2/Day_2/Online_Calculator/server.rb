cc826ef45984a41031d932794ed6ed0d78ed1f84
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-10-25 18:03:43 -0400

    home page now is centered and offers all 4 operations

--- Week_2/Day_2/Online_Calculator/server.rb
+++ Week_2/Day_2/Online_Calculator/server.rb
@@ -5,29 +5,13 @@ get "/" do
   erb(:home)
 end
 
-
 get "/add" do
   erb(:add)
 end
 
-post "/calculate_add" do
-  first = params[:first_number].to_f
-  second = params[:second_number].to_f
-  result = first + second
-  "The addition of #{first} and #{second} is #{result}."
-end
-
-
 
 get "/subtract" do
   erb(:subtract)
-end
-
-post "/calculate_subtract" do
-  first = params[:first_number].to_f
-  second = params[:second_number].to_f
-  result = first - second
-  "The subtraction of #{first} and #{second} is #{result}."
 end
 
 
@@ -35,21 +19,12 @@ get "/multiply" do
   erb(:multiply)
 end
 
-post "/calculate_multiply" do
-  first = params[:first_number].to_f
-  second = params[:second_number].to_f
-  result = first * second
-  "The multiplication of #{first} and #{second} is #{result}."
-end
-
 
 get "/divide" do
   erb(:divide)
 end
 
-post "/calculate_divide" do
-  first = params[:first_number].to_f
-  second = params[:second_number].to_f
-  result = first % second
-  "The division of #{first} and #{second} is #{result}."
-end
+
+params[:operation]
+
+
