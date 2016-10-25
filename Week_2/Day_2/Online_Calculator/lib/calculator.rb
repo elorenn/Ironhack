ae3b5f5d4632f6f1a441fd68f342d9881781285d
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-10-25 19:54:41 -0400

    calculator runs without errors, but it doesn't post result

--- Week_2/Day_2/Online_Calculator/lib/calculator.rb
+++ Week_2/Day_2/Online_Calculator/lib/calculator.rb
@@ -1,58 +1,28 @@
-require "sinatra"
-require_relative("../server.rb")
-
-
 class Calculator
+	
 
-	def addition(first_number, second_number)
-		first_number + second_number
+	def initialize(first_number, second_number)
+		@first_number = first_number.to_f
+		@second_number = second_number.to_f
+		
+	end	
+	def addition
+		result = @first_number + @second_number
+
 	end	
 
-	def subtraction(first_number, second_number)
-		first_number - second_number
+	def subtraction
+		result = @first_number - @second_number
 	end	
 
-	def multiplication(first_number, second_number)
-		first_number * second_number
+	def multiplication
+		result = @first_number * @second_number
 	end	
 
-	def division(first_number, second_number)
-		first_number / second_number
+	def division
+		result =@first_number / @second_number
 	end	
 
 end
-
-
-post "/calculate_add" do
-  operation = params[:addition]
-  first = params[:first_number].to_f
-  second = params[:second_number].to_f
-  result = first + second
-  "The addition of #{first} and #{second} is #{result}."
-end
-
-
-post "/calculate_subtract" do
-  operation = params[:subtraction]
-  first = params[:first_number].to_f
-  second = params[:second_number].to_f
-  result = first - second
-  "The subtraction of #{first} and #{second} is #{result}."
-end
-
-post "/calculate_multiply" do
-  operation = params[:multiplication]
-  first = params[:first_number].to_f
-  second = params[:second_number].to_f
-  result = first * second
-  "The multiplication of #{first} and #{second} is #{result}."
-end
-
-post "/calculate_divide" do
-  operation = params[:division]
-  first = params[:first_number].to_f
-  second = params[:second_number].to_f
-  result = first / second
-  "The division of #{first} and #{second} is #{result}."
-end
-
+jaa = Calculator.new("1","2")
+ jaa.addition
--- Week_2/Day_2/Online_Calculator/server.rb
+++ Week_2/Day_2/Online_Calculator/server.rb
@@ -2,27 +2,63 @@ require "sinatra"
 require_relative("lib/calculator.rb")
 
 get "/" do
-  erb(:home)
+  erb(:main_calculator)
 end
 
-get "/add" do
-  erb(:add)
+post "/calculator" do
+	calc = Calculator.new(params[:first_number], params[:second_number]) 
+
+	if params[:operation] == "addition"
+		
+		calc.addition
+
+
+	elsif params[:operation] == "subtraction"
+				
+		calc.subtraction
+	
+	elsif params[:operation] == "multiplication"
+										
+		calc.multiplication
+
+	elsif params[:operation] == "division"
+		
+		calc.division
+
+	end
+
+	# operation = params[:addition]
+	# first = params[:first_number].to_f
+	# second = params[:second_number].to_f
+	# result = first + second
+	# "The addition of #{first} and #{second} is #{result}."
 end
 
 
-get "/subtract" do
-  erb(:subtract)
-end
+# post "/calculate_subtract" do
+#   operation = params[:subtraction]
+#   first = params[:first_number].to_f
+#   second = params[:second_number].to_f
+#   result = first - second
+#   "The subtraction of #{first} and #{second} is #{result}."
+# end
 
+# post "/calculate_multiply" do
+#   operation = params[:multiplication]
+#   first = params[:first_number].to_f
+#   second = params[:second_number].to_f
+#   result = first * second
+#   "The multiplication of #{first} and #{second} is #{result}."
+# end
 
-get "/multiply" do
-  erb(:multiply)
-end
+# post "/calculate_divide" do
+#   operation = params[:division]
+#   first = params[:first_number].to_f
+#   second = params[:second_number].to_f
+#   result = first / second
+#   "The division of #{first} and #{second} is #{result}."
+# end
 
-
-get "/divide" do
-  erb(:divide)
-end
 
 
 
