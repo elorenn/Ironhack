e64a9bb3b805d435fc2f9f47d4632dc5bb6f8ce6
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-10-27 15:37:45 -0400

    online to do list looks a little nicer

--- Week_2/Day_3/More_To_Do/server.rb
+++ Week_2/Day_3/More_To_Do/server.rb
@@ -1,12 +1,29 @@
+# Right now our domain is localhost:4567
+
 require 'sinatra'
 require 'sinatra/reloader' if development?
-# We're going to need to require our class files
+# We're going to need to require our class files:
 require_relative('lib/Task.rb')
 require_relative('lib/to_do_list.rb')
 
+
 todo_list = ToDoList.new("Lorena")
-todo_list.load_tasks
-get "/tasks" do
+#todo_list.load_tasks
+
+get "/" do
+  erb(:home)
+end
+
+get "/list" do
+	#@tasks = list.tasks
   erb(:task_index)
 end
 
+get "/add_tasks" do
+	#@tasks = list.tasks
+  erb(:add_tasks)
+end
+
+post "/create_tasks" do
+	
+end
