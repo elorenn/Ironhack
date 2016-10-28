91c193b481a1d86186dc4d723ba3e812b1d07d5d
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-10-28 15:55:10 -0400

    movie trivia is online

--- Week_2/Day_5/movie_trivia/app.rb
+++ Week_2/Day_5/movie_trivia/app.rb
@@ -1,3 +1,4 @@
+require "bundler/setup"
 require "sinatra"
 require "sinatra/reloader" if development? 
 require "imdb"
@@ -11,5 +12,20 @@ get "/search_results" do
 
 	@movies = the_search.movies
 
-	erb :results 
+	erb(:results) 
 end
+
+post "/searching" do
+	
+	
+	redirect to ("/search_results")
+end
+
+
+# ask the user for a search term
+# returns 9 movies that match search term
+# then make sure the movies returned all have posters
+# then ask the user to guess which movie was release in x year
+# use Java to show correct result
+# show error if search returns less than 2 movies 
+# Bonus: choose randomly (shuffle?) bwtn asking to guess year and asking something else
