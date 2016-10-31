c15e19fb26536d89b08a2736726fba3cb03e04d7
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-10-30 22:45:39 -0400

    Ok...now it actually returns only 9 movie posters.

--- Week_2/Day_5/movie_trivia/app.rb
+++ Week_2/Day_5/movie_trivia/app.rb
@@ -18,12 +18,14 @@ get "/searching" do
 	the_search = Imdb::Search.new(params[:select_word])
 
 	@movies = the_search.movies
-	@twenty_movies = the_search.movies.take(20)
+	@twenty_movies = the_search.movies.take(14)
 
 	@nine_movies = []
 	@twenty_movies.each do |movie| 
-		if movie.poster != nil 
-			@nine_movies << movie 
+		unless @nine_movies.length >= 9
+			if movie.poster != nil 
+				@nine_movies << movie 
+			end
 		end
 	end
 				
@@ -40,6 +42,7 @@ end
 # ask the user for a search term
 # returns 9 movies that match search term
 # then make sure the movies returned all have posters
+
 # then ask the user to guess which movie was release in x year
 # use Java to show correct result
 # show error if search returns less than 2 movies 
