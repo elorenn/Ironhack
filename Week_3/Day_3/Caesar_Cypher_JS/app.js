eacdd6d5cf9da347bd0bffa1551e634937df3baf
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-11-02 18:28:20 -0400

    iteration 2 of Caesar's Cypher JavaScript

--- Week_3/Day_3/Caesar_Cypher_JS/app.js
+++ Week_3/Day_3/Caesar_Cypher_JS/app.js
@@ -1,16 +1,60 @@
+// --------------------------------------------------------------
+//-------------------- Iteration 1 -------------------------------
+// --------------------------------------------------------------
+
+// function caesarCipher (message) {
+// 	var letter_array = message.split("");
+// 	var changed_letters = []
+
+// 	letter_array.forEach(function (letter) {
+// 		changed_letters.push(String.fromCharCode((letter.charCodeAt(0)) - 3));	
+// 	});
+
+// 	return changed_letters.join("");
+// }
+
+
+// var encrypted = caesarCipher("brutus");
+
+// console.log(encrypted);
+// "brutus" => "_orqrp"
+
+
+// --------------------------------------------------------------
+
+			// try it with .map: 
+
+
+// --------------------------------------------------------------
+//-------------------- Iteration 2 ------------------------------
+// --------------------------------------------------------------
+
 function caesarCipher (message) {
 	var letter_array = message.split("");
 	var changed_letters = []
 
+
 	letter_array.forEach(function (letter) {
-		changed_letters.push(String.fromCharCode((letter.charCodeAt(0)) - 3));	
+		if ((letter >= "A" && letter <= "Z") || (letter >= "a" && letter <= "z")) {
+			changed_letters.push(String.fromCharCode((letter.charCodeAt(0)) - 3));
+		} else {
+			changed_letters.push(letter);
+		}	
 	});
 
+
 	return changed_letters.join("");
-}
+} 
 
 
-var encrypted = caesarCipher("brutus");
+var encrypted = caesarCipher("Et tu, brute?");
 
 console.log(encrypted);
-// "brutus" => "_orqrp"
+//=> "Bq qr, _orqb?"
+
+
+
+
+
+
+
