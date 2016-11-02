59c9467e3a9726167cea744d06bcfa612ba6b8fc
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-11-02 14:41:59 -0400

    made JS_functions app.js a bit easier to read

--- Week_3/Day_3/JS_functions/app.js
+++ Week_3/Day_3/JS_functions/app.js
@@ -3,23 +3,26 @@ function averageColon (string) {
 	// string_array = [ '80', '70', '90', '100' ]
 
 	var number_array = []
-	// number_array = [ 80, 70, 90, 100 ]
 
 	string_array.forEach(function (eachString) {
 		eachNumber = Math.floor(eachString)
 		number_array.push(eachNumber);
 	});
+	// number_array = [ 80, 70, 90, 100 ]
 
 	var total = 0
 	for(var i = 0; i < number_array.length; i ++) {
 		total += number_array[i];
-		console.log(total);
-		// total = 340
 	}
+
+	console.log(total);
+	// total = 340
+
 	var average = total / number_array.length;
 	console.log(average);
-	return average;
 	// average = 85
+	return average
+	// you must "return average" in order for test to run "true"
 }
 
 
