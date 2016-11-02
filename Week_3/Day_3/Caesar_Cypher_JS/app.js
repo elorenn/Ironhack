dd1bdc72dbbe1ad36699323aefa42817a99f4f93
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-11-02 19:17:50 -0400

    iteration 3 of Caesar's Cipher JavaScript

--- Week_3/Day_3/Caesar_Cypher_JS/app.js
+++ Week_3/Day_3/Caesar_Cypher_JS/app.js
@@ -29,14 +29,43 @@
 //-------------------- Iteration 2 ------------------------------
 // --------------------------------------------------------------
 
-function caesarCipher (message) {
+// function caesarCipher (message) {
+// 	var letter_array = message.split("");
+// 	var changed_letters = []
+
+
+// 	letter_array.forEach(function (letter) {
+// 		if ((letter >= "A" && letter <= "Z") || (letter >= "a" && letter <= "z")) {
+// 			changed_letters.push(String.fromCharCode((letter.charCodeAt(0)) - 3));
+// 		} else {
+// 			changed_letters.push(letter);
+// 		}	
+// 	});
+
+
+// 	return changed_letters.join("");
+// } 
+
+
+// var encrypted = caesarCipher("Et tu, brute?");
+
+// console.log(encrypted);
+// //=> "Bq qr, _orqb?"
+
+
+// --------------------------------------------------------------
+//-------------------- Iteration 3 ------------------------------
+// --------------------------------------------------------------
+
+
+function caesarCipher (message, shift) {
 	var letter_array = message.split("");
 	var changed_letters = []
 
 
 	letter_array.forEach(function (letter) {
 		if ((letter >= "A" && letter <= "Z") || (letter >= "a" && letter <= "z")) {
-			changed_letters.push(String.fromCharCode((letter.charCodeAt(0)) - 3));
+			changed_letters.push(String.fromCharCode((letter.charCodeAt(0)) + shift));
 		} else {
 			changed_letters.push(letter);
 		}	
@@ -46,15 +75,17 @@ function caesarCipher (message) {
 	return changed_letters.join("");
 } 
 
-
-var encrypted = caesarCipher("Et tu, brute?");
+// Left shift of 4
+var encrypted = caesarCipher("Et tu, brute?", -4);
 
 console.log(encrypted);
-//=> "Bq qr, _orqb?"
+//=> "Ap pq, ^nqpa?"
 
+// Right shift of 4
+encrypted = caesarCipher("Et tu, brute?", 6);
 
-
-
+console.log(encrypted);
+//=> "Kz z{, hx{zk?"
 
 
 
