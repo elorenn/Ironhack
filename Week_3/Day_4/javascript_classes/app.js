d0683a026130f2a1cd7239cbda0bebb7cb370b90
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-11-03 10:11:57 -0400

    less bad version of js app

--- Week_3/Day_4/javascript_classes/app.js
+++ Week_3/Day_4/javascript_classes/app.js
@@ -1,35 +1,55 @@
-var thunder = {
-	name: "Thunder",
-	breed: "German Shepherd",
-	sound: "woof?",
+"use strict";
 
-	chase: function () {
-		console.log(`${this.name} is chasing cars.`);
-	},
+var Animal = require("./lib/animal.js");
 
-	bark: function () {
-		console.log(this.sound);
-	}
-};
+console.log("");
+
+var thunder = new Animal("Thunder", "German Shepherd", "woof?")
 
 thunder.chase();
 thunder.bark();
 
 console.log("");
 
-var bear = {
-	name: "Bear",
-	breed: "Border Collie",
-	sound: "uoooa?",
+var bear = new Animal("Bear", "Border Collie", "uoooa")
 
-	chase: function () {
-		console.log(`${this.name} is chasing cars.`);
-	},
-
-	bark: function () {
-		console.log(this.sound);
-	}
-};
 
 bear.chase();
 bear.bark();
+
+
+// var thunder = {
+// 	name: "Thunder",
+// 	breed: "German Shepherd",
+// 	sound: "woof?",
+
+// 	chase: function () {
+// 		console.log(`${this.name} is chasing cars.`);
+// 	},
+
+// 	bark: function () {
+// 		console.log(this.sound);
+// 	}
+// };
+
+// thunder.chase();
+// thunder.bark();
+
+// console.log("");
+
+// var bear = {
+// 	name: "Bear",
+// 	breed: "Border Collie",
+// 	sound: "uoooa?",
+
+// 	chase: function () {
+// 		console.log(`${this.name} is chasing cars.`);
+// 	},
+
+// 	bark: function () {
+// 		console.log(this.sound);
+// 	}
+// };
+
+// bear.chase();
+// bear.bark();
