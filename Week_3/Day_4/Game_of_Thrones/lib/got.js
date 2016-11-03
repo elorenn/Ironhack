7e743cbff345bf63bf742290d2c7bda4be52a8e0
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-11-03 15:39:10 -0400

    now prints out all the episodes along with rounded star ratings

--- Week_3/Day_4/Game_of_Thrones/lib/got.js
+++ Week_3/Day_4/Game_of_Thrones/lib/got.js
@@ -9,17 +9,24 @@ function fileActions(err, file){
     }
    var episodes = JSON.parse(file);
 
-   afterSearch(episodes, 0);
+   afterSearch(episodes);
 }
 
 fs.readFile("./GoTEpisodes.json", 'utf8', fileActions);
 
 
-function afterSearch(ep, n) {
+// function afterSearch(ep, n) {
+// 	console.log(`Title: ${ep[n].title} Episode #: ${ep[n].episode_number}`);
+// 	console.log(ep[n].description); 
+// 	//console.log(`Rating:` + ep[n].rating + " " + "*".repeat(ep[n].rating) );
+// 	console.log(`Rating:` + ep[n].rating + " " + "*".repeat(Math.round(ep[n].rating)) );
+// }
 
-	console.log(`Title: ${ep[n].title} Episode #: ${ep[n].episode_number}`);
-	console.log(ep[n].description); 
-	//console.log(`Rating:` + ep[n].rating + " " + "*".repeat(ep[n].rating) );
-	console.log(`Rating:` + ep[n].rating + " " + "*".repeat(Math.round(ep[n].rating)) );
+function afterSearch(ep) {
+	ep.forEach(function(anEpisode) {
+		console.log("")
+		console.log(`Title: ${anEpisode.title} Episode #: ${anEpisode.episode_number}`);
+		console.log(anEpisode.description); 
+		console.log(`Rating:` + anEpisode.rating + " " + "*".repeat(Math.round(anEpisode.rating)) );
+	});	
 }
-
