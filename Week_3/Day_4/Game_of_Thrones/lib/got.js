5febc8aecac1702364339067517940d62a9460f5
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-11-03 17:14:49 -0400

    iteration 4 - only prints out if rating is 8.5 or higher

--- Week_3/Day_4/Game_of_Thrones/lib/got.js
+++ Week_3/Day_4/Game_of_Thrones/lib/got.js
@@ -9,32 +9,55 @@ function fileActions(err, file){
     }
    var episodes = JSON.parse(file);
 
-   afterSearch(episodes);
+   sortEpisodes(episodes);
+  
 }
 
 fs.readFile("./GoTEpisodes.json", 'utf8', fileActions);
 
 
-// function afterSearch(ep, n) {
-// 	console.log(`Title: ${ep[n].title} Episode #: ${ep[n].episode_number}`);
-// 	console.log(ep[n].description); 
-// 	//console.log(`Rating:` + ep[n].rating + " " + "*".repeat(ep[n].rating) );
-// 	console.log(`Rating:` + ep[n].rating + " " + "*".repeat(Math.round(ep[n].rating)) );
-// }
-
-function afterSearch(ep) {
-	var sortedEpisode = ep.sort(function(a, b){
+function sortEpisodes(ep) {
+	 ep.sort(function(a, b){
 		return a.episode_number - b.episode_number
 	});
 
-	ep.forEach(function(anEpisode) {
+	function highRatings(value) {
+		return value.rating >= 8.5;
+	}
+	var filtered = ep.filter(highRatings);
+
+	function printStuff(anEpisode){
 		console.log("")
 		console.log(`Title: ${anEpisode.title} Episode #: ${anEpisode.episode_number}`);
 		console.log(anEpisode.description); 
 		console.log(`Rating:` + anEpisode.rating + " " + "*".repeat(Math.round(anEpisode.rating)) );
-	});	
+		
+	}
+
+	filtered.forEach(printStuff)
 }
 
+
+
+
+// function highestRatings(ep) {
+// 	var highRatingEp = []
+// 	ep.forEach(x) {
+// 		if (x.rating >= 8.5) {
+// 			highRatingEp.push(x);	
+// 		}};
+// 	console.log("");
+// 	console.log("Episodes with Ratings of 8.5 or Above:");
+// 	console.log("-----------------------------------------");
+// 	console.log("");	
+// 	highRatingEp.forEach(printStuff);
+// }
+
+
+
+// --------------------------------------------------------------
+// ------- Using a for loop instead of forEach ------------------
+// --------------------------------------------------------------
 
 // function afterSearch(ep) {
 //     for (i = 0; i < ep.length; i++) {
@@ -43,4 +66,15 @@ function afterSearch(ep) {
 //     console.log('Rating: ' + ep[i].rating + " " + '*'.repeat(Math.round(ep[i].rating)));
 // }}
 
+// --------------------------------------------------------------
+// --------------------------------------------------------------
+// --------------------------------------------------------------
+
+
+// function afterSearch(ep, n) {
+// 	console.log(`Title: ${ep[n].title} Episode #: ${ep[n].episode_number}`);
+// 	console.log(ep[n].description); 
+// 	//console.log(`Rating:` + ep[n].rating + " " + "*".repeat(ep[n].rating) );
+// 	console.log(`Rating:` + ep[n].rating + " " + "*".repeat(Math.round(ep[n].rating)) );
+// }
 
