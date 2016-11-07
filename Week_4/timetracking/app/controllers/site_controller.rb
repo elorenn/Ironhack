4935b4f50450d357feb5ef31adfbfa77f2ef69b9
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-11-07 11:12:07 -0500

    changes to rails practice

---
+++ Week_4/timetracking/app/controllers/site_controller.rb
@@ -0,0 +1,10 @@
+class SiteController < ApplicationController
+
+	def home
+		render 'home' #name of view template
+	end
+
+	def contact
+		render 'contact' 
+	end
+end
--- Week_4/timetracking/config/routes.rb
+++ Week_4/timetracking/config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+# these are all acceptable ways of writing the same thing:
+# how to get to the homepage:
+  get("/", { :to => "site#home"})
+  get("/", { to: "site#home"})
+  get "/", { to: "site#home"}
+  get "/", :to => "site#home"
+
+  get "/about", :to => "site#about"
+
+  get "/contact", :to => "site#contact"
+
 end
