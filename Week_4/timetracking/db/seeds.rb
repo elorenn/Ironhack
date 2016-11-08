e114361e1780fee524ee4f236dc119e757331bae
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-11-08 15:57:21 -0500

    created seeds - 10 projects

--- Week_4/timetracking/db/seeds.rb
+++ Week_4/timetracking/db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+projectA = Project.create!(name: "Ironhack")
+projectB = Project.create!(name: "Edwardian Chairs", description: "Refurbing those two old chairs.")
+projectC = Project.create!(name: "Plastic Pinks Album", description: "Artwork for new record")
+projectD = Project.create!(name: "RTW", description: "plans for round the world trip")
+projectE = Project.create!(name: "Underwater Globe", description: "Blueprints for spherical underwater home")
+projectF = Project.create!(name: "Straddle Bus", description: "plans for new transportation method")
+projectG = Project.create!(name: "Some other Project", description: "ideas for more projects")
+projectH = Project.create!(name: "Juggalize-Me!", description: "app that superimposed juggalo makeup onto a photo of your face")
+projectI = Project.create!(name: "Tailor Made", description: "app that connects you with a tailor")
+projectJ = Project.create!(name: "Where do I go", description: "app that buys you a random plane ticket within a certain budget on a certain date")
+	
