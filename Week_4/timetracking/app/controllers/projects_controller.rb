1fd8f5c6ff6df6494c5af8d2bbb9b98744497e7f
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-11-09 10:36:55 -0500

    learning url parameters and ids. CRUD

--- Week_4/timetracking/app/controllers/projects_controller.rb
+++ Week_4/timetracking/app/controllers/projects_controller.rb
@@ -1,8 +1,16 @@
 class ProjectsController < ApplicationController
 
-	def index
-		
+	def index	
 		@projects = Project.order(created_at: :desc).limit(10)
 
+		render :index
+	end
+
+	def show
+		@my_project = Project.find(params[:id])
+
+# in Rails, the render here is implicit
+# we're writing it in anyway for the sake of clarity while learning
+		render :show 
 	end
 end
--- Week_4/timetracking/config/routes.rb
+++ Week_4/timetracking/config/routes.rb
@@ -21,4 +21,12 @@ Rails.application.routes.draw do
 
   get '/projects', to: 'projects#index'  
 
+# the colon (:) before id means that it's a placeholder
+# in the url, :id will be replaced with each corresponding id value
+# if you try to visit the page, you have to plug in a read id value number
+
+#                                              project path
+#                                                   |
+  get '/projects/:id', to: 'projects#show', as: "project" 
+
 end
