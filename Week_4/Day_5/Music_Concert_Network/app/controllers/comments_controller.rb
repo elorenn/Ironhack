77c17cbfa971edd5b1770c6d6e73bb4c022dd174
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-11-14 01:07:13 -0500

    user can now create new comments. - validations in place with error messages. - can see and hide all comments

--- Week_4/Day_5/Music_Concert_Network/app/controllers/comments_controller.rb
+++ Week_4/Day_5/Music_Concert_Network/app/controllers/comments_controller.rb
@@ -5,4 +5,32 @@ class CommentsController < ApplicationController
 		@the_comments = @the_concert.comments 
 		render 'index'
 	end
+
+	def new
+		@the_concert = Concert.find(params[:concert_id])
+		@the_comment  = @the_concert.comments.new 
+
+		render 'new'
+	end
+
+	def create
+		@the_concert = Concert.find(params[:concert_id])
+		@the_comment = @the_concert.comments.new(
+			:user => params[:comment][:user],
+			:comment => params[:comment][:comment],
+			:date => params[:comment][:date])
+
+		if @the_comment.save
+
+			redirect_to concert_path(@the_concert)
+		else
+			render 'new'
+		end
+	end
+
+	private
+
+	def entry_params
+		params.require(:comment).permit(:user,:comment)
+	end
 end
--- Week_4/Day_5/Music_Concert_Network/app/models/comment.rb
+++ Week_4/Day_5/Music_Concert_Network/app/models/comment.rb
@@ -1,3 +1,6 @@
 class Comment < ApplicationRecord
-	belongs_to :project 
+	validates :user, presence: true
+	validates :comment, presence: true
+
+	belongs_to :concert 
 end
