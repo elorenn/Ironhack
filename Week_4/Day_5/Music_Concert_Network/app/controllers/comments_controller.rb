efb1618d68427e098ed02e909542fcba8de73af3
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-11-14 00:05:18 -0500

    updated routes to use resources. fixed resulting broken links.

--- Week_4/Day_5/Music_Concert_Network/app/controllers/comments_controller.rb
+++ Week_4/Day_5/Music_Concert_Network/app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
 
 	def index
-		@the_concert = Concert.find(params[:id])
+		@the_concert = Concert.find(params[:concert_id])
 		@the_comments = @the_concert.comments 
 		render 'index'
 	end
--- Week_4/Day_5/Music_Concert_Network/config/routes.rb
+++ Week_4/Day_5/Music_Concert_Network/config/routes.rb
@@ -3,13 +3,20 @@ Rails.application.routes.draw do
 
 	get "/", to: "site#home"
 
-	get "/concerts", to: "concerts#index"
+	# get "/concerts", to: "concerts#index"
 
-	get "/concerts/new", to: "concerts#new"
- 	post "/concerts", to: "concerts#create"
+	# get "/concerts/new", to: "concerts#new"
+ 	# post "/concerts", to: "concerts#create"
 
- 	get '/concerts/:id', to: 'concerts#show', as: :concert_show
+ 	# get '/concerts/:id', to: 'concerts#show', as: :concert_show
 
- 	get '/concerts/:id/comments', to: 'comments#index', as: :concert_comments
+ 	# get '/concerts/:id/comments', to: 'comments#index', as: :concert_comments
+
+ 	# get '/concerts/:id/comments/new', to: 'comments#new', as: :new_concert_comments
+
+
+ 	resources :concerts, only: [:index, :show, :new, :create] do 
+    resources :comments, except: [:show]
+   end  
 
 end
