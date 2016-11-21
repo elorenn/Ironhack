cf67a823787e5497b1af100a9a21daae48df8255
Lorena Sarasua <lorena.sarasua@gmail.com>
2016-11-20 22:49:21 -0500

    working on spotify player

--- Week_5/Day_1/jquery-exercise/app.js
+++ Week_5/Day_1/jquery-exercise/app.js
@@ -3,13 +3,13 @@ console.log("CONSOLE LOG");
 $(document).ready(function () {
 
 	var phrases = [
-		'There is a loneliness in this world so great that you can see it in the slow movement of the hands of a clock.',
-		'Perhaps everybody has a garden of Eden.',
-		'They slipped briskly into an intimacy from which they never recovered.',
-		'For all that we love deeply, becomes a part of us.',
-		'Life is either a daring adventure, or nothing at all.',
-		'Human life is but a series of footnotes to a vast obscure unfinished masterpiece.',
-    'Like failure, chaos contains information that can lead to knowledge — even wisdom.'
+		'There is a loneliness in this world so great that you can see it in the slow movement of the hands of a clock. ',
+		'Perhaps everybody has a garden of Eden. ',
+		'They slipped briskly into an intimacy from which they never recovered. ',
+		'For all that we love deeply, becomes a part of us. ',
+		'Life is either a daring adventure, or nothing at all. ',
+		'Human life is but a series of footnotes to a vast obscure unfinished masterpiece. ',
+    'Like failure, chaos contains information that can lead to knowledge - even wisdom. ' 
 
 	]; 
 
